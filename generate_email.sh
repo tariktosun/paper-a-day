ad0a960f7f5ad36104a8080d4918138c35063002
Tarik Tosun <tariktosun@crossbow.dreamhost.com>
2018-01-03 16:35:37 -0800

    full paths in generate_email.sh.  It works now!

--- generate_email.sh
+++ generate_email.sh
@@ -1,2 +1,2 @@
-source paper-a-day_env/bin/activate
-python generate_email.py 
+source /home/tariktosun/tariktosun.com/paper-a-day/paper-a-day_env/bin/activate
+python /home/tariktosun/tariktosun.com/paper-a-day/generate_email.py 
